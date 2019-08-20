d66323f2a0beb55f396141b8a925f304ddd4154f
Ujjwal Batra <ujjwalbatra2509@gmail.com>
2019-08-20 16:10:08 +1000

    initial commit

---
+++ src/main/java/edu/rmit/cosc2367/s3673681/RecommenderSystem.java
@@ -0,0 +1,33 @@
+package edu.rmit.cosc2367.s3673681;
+
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Hello world!
+ */
+public class RecommenderSystem {
+    public static void main(String[] args) throws Exception{
+        DataModel model = new FileDataModel(new File("dataset.csv"));
+        UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+
+        UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
+        UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+
+        List<RecommendedItem> recommendations = recommender.recommend(2, 3);
+        for (RecommendedItem recommendation : recommendations) {
+            System.out.println(recommendation);
+        }
+
+    }
+}
